d78d3ab45c1ca3558121980b1d46dccf013002ad
lady1bug <107481455+lady1bug@users.noreply.github.com>
2026-05-01 18:18:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,34 @@ class MenuScreen(Screen):
        App.get_running_app().stop()
 
 
+class Fish(Image):
+    hp =0
+    fish_index = 0
+    def new_fish(self, *args):
+        app = App.get_running_app()
+        # Беремо дані про поточну рибу
+        fish_key = app.LEVELS[app.LEVEL][self.fish_index]
+        self.source = app.FISHES[fish_key]['source']
+        self.hp = app.FISHES[fish_key]['hp']
+        self.opacity = 1
+
+    def on_touch(self):
+        if self.collide_point(*self.touch_pos) or self.opacity == 0:
+            return False
+        self.hp -= 1
+        game_screen = App.get_running_app().root.root.get_screen('game')
+        game_screen.score += 1
+        if self.hp <= 0:
+            self.opacity = 0
+            app = App.get_running_app()
+            if len(app.LEVELS[app.LEVEL]) > self.fish_index + 1:
+                self.fish_index += 1
+                Clock.schedule_once(self.new_fish, 1.2)
+            else:
+                Clock.schedule_once(game_screen.level_complete, 1.2)
+                self.fish_index = 0
+            return True
+
 
 
 class GameScreen(Screen):
@@ -45,12 +73,18 @@ class SettingsScreen(Screen):
 
 # Головний застосунок
 class ClickerApp(App):
-   def build(self):
-       sm = ScreenManager()
-       sm.add_widget(MenuScreen(name="menu"))
-       sm.add_widget(GameScreen(name="game"))
-       sm.add_widget(SettingsScreen(name="settings"))
-       return sm
+    LEVEL = 0
+    FISHES = {
+        'fish1': {'source': 'images/fish_01.png', 'hp': 10},
+        'fish2': {'source': 'images/fish_02.png', 'hp': 20}
+    }
+    LEVELS = [['fish1', 'fish1', 'fish2']]
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name="menu"))
+        sm.add_widget(GameScreen(name="game"))
+        sm.add_widget(SettingsScreen(name="settings"))
+        return sm
 
 
 
